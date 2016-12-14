088487d2e707a3a22e7ad9cc0c93aa1a06b1d76f
sindycarlo <sindycarlo@gmail.com>
2016-12-14 16:34:10 +0100

    compilazione parte logica

--- main.cpp
+++ main.cpp
@@ -14,7 +14,16 @@
 int main(int argc, char *argv[])
 {
     //QApplication a(argc, argv);
-    rivista x();
+    rivista* x=new rivista("alice nel paese delle cacche",2004,true);
+    rivista* y=new rivista("alice nel paese delle cacche",2004,true);
+    rivista* z=new rivista("alice nel paese delle cacche",2004,true);
+
+    contenitore diprova;
+    diprova.add_item(x);
+    diprova.add_item(x);
+    diprova.add_item(y);
+    diprova.add_item(z);
+
 
    // return a.exec();
 }
