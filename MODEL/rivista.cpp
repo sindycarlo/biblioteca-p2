c1cf1f8888dd2f9af77b0aed03bfdda761ad8756
sindycarlo <sindycarlo@gmail.com>
2016-12-14 09:27:42 +0100

    rivista.cpp costruttore da definire

--- MODEL/rivista.cpp
+++ MODEL/rivista.cpp
@@ -3,3 +3,13 @@
 #include <QXmlStreamWriter>
 #include<QString>
 #include<iostream>
+
+unsigned int rivista::MaxAnni=20;
+
+rivista::rivista(QString t, unsigned int au, bool d) : opera(t,d), annouscita(au) {
+    if(annouscita>1000)
+    {std::cout<<"Errore: anno di uscita dela rivista troppo grande";}
+    annouscita=1000;
+}
+
+
